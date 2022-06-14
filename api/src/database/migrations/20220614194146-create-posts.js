c2b8979c4227e0e3837b20897638aabd4fe17c5d
Cintia Macchia <macchiapaola@hotmail.com>
2022-06-14 16:54:25 -0300

    adicionando conexão BD e migrations

---
+++ api/src/database/migrations/20220614194146-create-posts.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Posts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            content: {
+                type: Sequelize.STRING
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                reference: {
+                    model: {
+                        tableName: 'Users',
+                        schema: 'parrot'
+                    },
+                    key: 'id'
+                },
+                allowNull: false,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Posts');
+    }
+};
